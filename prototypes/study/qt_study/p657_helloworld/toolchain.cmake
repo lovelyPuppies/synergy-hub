0b096d226ac6edbd9fdf1205b09bba50d6d123bc
wbfw109v2 <wbfw109v2@gmail.com>
2025-01-06 03:26:10 +0900

    .

--- prototypes/study/qt_study/p657_helloworld/toolchain.cmake
+++ prototypes/study/qt_study/p657_helloworld/toolchain.cmake
@@ -1,12 +1,14 @@
+# 시스템 및 아키텍처 설정
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# 크로스 컴파일러 설정
-set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
+# Clang 기반 크로스 컴파일러 설정
+set(CMAKE_C_COMPILER /usr/bin/clang)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++)
 
-# sysroot 설정
-set(CMAKE_SYSROOT /home/wbfw109v2/rpi-sysroot)
+# Clang 크로스 컴파일 타겟 및 sysroot 설정
+set(CMAKE_C_FLAGS "--target=arm-linux-gnueabihf --sysroot=/home/wbfw109v2/rpi-sysroot")
+set(CMAKE_CXX_FLAGS "--target=arm-linux-gnueabihf --sysroot=/home/wbfw109v2/rpi-sysroot")
 
 # Qt 경로 설정
 set(CMAKE_PREFIX_PATH /home/wbfw109v2/qt-platforms/qt6.8.0-arm32v7-bookworm)
@@ -17,6 +19,29 @@ set(CMAKE_FIND_ROOT_PATH /home/wbfw109v2/rpi-sysroot)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# # 시스템 및 아키텍처 설정
+# set(CMAKE_SYSTEM_NAME Linux)
+# set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# # Clang 기반 크로스 컴파일러 설정
+# set(CMAKE_C_COMPILER /usr/bin/clang)
+# set(CMAKE_CXX_COMPILER /usr/bin/clang++)
+
+# # Clang 크로스 컴파일 타겟 및 sysroot 설정
+# set(CMAKE_C_FLAGS "--target=arm-linux-gnueabihf --sysroot=/home/wbfw109v2/rpi-sysroot")
+# set(CMAKE_CXX_FLAGS "--target=arm-linux-gnueabihf --sysroot=/home/wbfw109v2/rpi-sysroot")
+
+# # Qt 경로 설정
+# set(CMAKE_PREFIX_PATH /home/wbfw109v2/qt-platforms/qt6.8.0-arm32v7-bookworm)
+# set(QT_HOST_PATH /home/wbfw109v2/Qt/6.8.0/gcc_64)
+
+# # 크로스 컴파일 환경에서 라이브러리 및 헤더 검색 설정
+# set(CMAKE_FIND_ROOT_PATH /home/wbfw109v2/rpi-sysroot)
+# set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+# set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
 
 
 # ✅ (How-to) cross compile to raspberry pi 4 B
