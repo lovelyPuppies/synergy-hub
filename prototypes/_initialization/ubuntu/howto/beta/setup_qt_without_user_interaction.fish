6b4504713aefd115c6edb59e74fe43b8085bc391
wbfw109v2 <wbfw109v2@gmail.com>
2025-01-12 20:12:44 +0900

    .

--- prototypes/_initialization/ubuntu/howto/beta/setup_qt_without_user_interaction.fish
+++ prototypes/_initialization/ubuntu/howto/beta/setup_qt_without_user_interaction.fish
@@ -1,28 +1,33 @@
 #!/usr/bin/env fish
+function on_interrupt
+    echo -e "\nScript interrupted. Exiting..."
+    # Kill all child processes in the same process group
+    kill -- -$fish_pid
+    exit 1
+end
+trap on_interrupt SIGINT
 
-# wget -O ~/Downloads/qt-unified https://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
-wget https://download.qt.io/official_releases/online_installers/qt-unified-linux-arm64-online.run
-chmod 700 ~/Downloads/qt-unified
-## if in ARM
-# wget https://download.qt.io/official_releases/online_installers/qt-unified-linux-arm64-online.run
-~/Downloads/qt-unified-linux-x64-online.run
 
+#####
+# https://download.qt.io/official_releases/online_installers
+# https://doc.qt.io/qt-6/get-and-install-qt-cli.html#installing-with-user-interaction
+echo "⌨️  Enter your Qt account email: "
+read qt_email
+echo ""
 
 echo "⌨️  Enter your Qt account password: "
 read -s qt_pw
 echo ""
 
-wget -O ~/Downloads/qt-unified https://download.qt.io/official_releases/online_installers/qt-unified-linux-x64-online.run
+wget -O ~/Downloads/qt-unified https://download.qt.io/official_releases/online_installers/qt-unified-linux-arm64-online.run
 chmod 700 ~/Downloads/qt-unified
 
 set qt_installer "$HOME/Downloads/qt-unified"
 set install_dir "$HOME/Qt"
-set qt_email "wbfw109v2@gmail.com"
 
 echo Accept | $qt_installer \
     --root $install_dir \
     --email $qt_email \
     --pw $qt_pw \
     --accept-licenses --default-answer --confirm-command \
-    install qt6.8.0-full
-# qt6.8.0-full-dev
+    install qt6.8.1-full
