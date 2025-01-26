58aa5963dc839847d19013b7bff350a76f1a7d90
wbfw109v2 <wbfw109v2@gmail.com>
2025-01-26 23:46:00 +0900

    .

--- prototypes/_initialization/ubuntu/howto/snippet.fish
+++ prototypes/_initialization/ubuntu/howto/snippet.fish
@@ -1,9 +1,3 @@
-## 🌀 Title: View Standard Output in VS Code 📅 2024-12-12 00:52:53
-##  Format: <stdout> > /tmp/out.txt; and code /tmp/out.txt
-# 🛍️ e.g.
-man systemctl >/tmp/man.txt 2>&1; and code /tmp/man.txt
-
-
 ## 🌀 Title: open .tex file 📅 2024-09-12 00:45:30
 code **/*.tex
 ##  🌀 Title: replace all .tex file with .txt file 📅 2024-09-12 00:45:30
