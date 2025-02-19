2e6e8c16cb3bee95faba7d007b68668c080145f3
wbfw109v2 <wbfw109v2@gmail.com>
2025-02-20 01:11:18 +0900

    .

--- study/cpp_study/vcpkg_study/main.cpp
+++ study/cpp_study/vcpkg_study/main.cpp
@@ -1,6 +1,11 @@
+#include <fmt/color.h>
 #include <fmt/core.h>
 
 int main() {
-  fmt::print("Hello, {}!\n", "Vcpkg with CMake");
+  fmt::print(fmt::fg(fmt::color::green), "Hello, {}!\n", "Vcpkg with CMake");
+  fmt::print(fmt::fg(fmt::color::red) | fmt::emphasis::bold,
+             "Error: Something went wrong!\n");
+  fmt::print(fmt::fg(fmt::color::blue) | fmt::emphasis::italic,
+             "Info: Running in debug mode\n");
   return 0;
 }
