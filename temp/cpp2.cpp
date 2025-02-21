985013f1c75760af7bb146610b87f1cc29aaf3fb
wbfw109v2 <wbfw109v2@gmail.com>
2025-02-21 19:41:08 +0900

    .

--- temp/cpp2.cpp
+++ temp/cpp2.cpp
@@ -1,78 +1,20 @@
-// TCMalloc Test: clang++ -o cpp cpp.cpp -ltcmalloc_minimal && ./cpp && ldd ./cpp | grep tcmalloc
-#include <iomanip>
+// sudo apt install mingw-w64
+// clang++ --target=x86_64-w64-mingw32 -o cpp2.exe cpp2.cpp
+// 🧪📵 clang++ --target=x86_64-pc-windows-msvc -o cpp2.exe cpp2.cpp
+// scp wbfw109v2@<ip>:... ...
+/*
+
+❯ file cpp2.exe
+cpp2.exe: PE32+ executable (console) x86-64, for MS Windows, 19 sections
+
+❯ wine cpp2.exe
+00f0:err:module:import_dll Library libstdc++-6.dll (which is needed by L"Z:\\home\\wbfw109v2\\repos\\synergy-hub\\temp\\cpp2.exe") not found
+00f0:err:module:loader_init Importing dlls for L"Z:\\home\\wbfw109v2\\repos\\synergy-hub\\temp\\cpp2.exe" failed, status c0000135
+*/
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
 
-using namespace std;
+int main() {
+  std::cout << "Hello world" << std::endl;
 
-int get_seconds(const string &min_sec) {
-  int minutes, seconds;
-  sscanf(min_sec.c_str(), "%d:%d", &minutes, &seconds);
-  return minutes * 60 + seconds;
+  return 0;
 }
-
-string format_time(int total_seconds) {
-  auto result = std::div(total_seconds, 60);
-  stringstream ss;
-  ss << setfill('0') << setw(2) << result.quot << ":" << setw(2) << result.rem;
-  return ss.str();
-}
-string solution(string video_len, string pos, string op_start, string op_end,
-                vector<string> commands) {
-  int video_len_int = get_seconds(video_len);
-  int op_start_int = get_seconds(op_start);
-  int op_end_int = get_seconds(op_end);
-  int pos_int = get_seconds(pos);
-
-  auto move_to_opend = [&](int sec) -> int {
-    if (op_start_int <= sec && sec <= op_end_int) {
-      return op_end_int;
-    }
-    return sec;
-  };
-
-  pos_int = move_to_opend(pos_int);
-
-  for (const auto &command : commands) {
-    if (command == "next") {
-      pos_int += 10;
-      if (pos_int > video_len_int - 10) {
-        pos_int = video_len_int;
-      }
-    } else if (command == "prev") {
-      pos_int -= 10;
-      if (pos_int < 10) {
-        pos_int = 0;
-      }
-    }
-    pos_int = move_to_opend(pos_int);
-  }
-
-  return format_time(pos_int);
-}
-
-// class Test {
-// public:
-//   int value;
-//   Test(int v) : value(v) {
-//     std::cout << "Test 객체 생성: " << value << std::endl;
-//   }
-//   ~Test() { std::cout << "Test 객체 소멸: " << value << std::endl; }
-// };
-
-// int main() {
-//   std::cout << "TCMalloc 테스트: new/delete 사용" << std::endl;
-
-//   // new를 사용하여 객체 할당
-//   Test *obj = new Test(42);
-
-//   // 객체 출력
-//   std::cout << "Test 객체 값: " << obj->value << std::endl;
-
-//   // 객체 해제
-//   delete obj;
-
-//   return 0;
-// }
